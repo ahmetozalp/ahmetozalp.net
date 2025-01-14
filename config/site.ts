3f401c2b74510c76f3514ffcededab09f7143a8d
ahmetozalp <ahmetozalpjson@gmail.com>
2025-01-14 19:47:45 +0300

    refactor: update site configuration with new branding and localized footer links

--- config/site.ts
+++ config/site.ts
@@ -4,30 +4,30 @@ import { env } from "@/env.mjs";
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  name: "Next Starter",
+  name: "Ahmet OZALP",
   description:
     "Get your project off to an explosive start with Auth & User Roles! Harness the power of Next.js 14, Prisma, Neon, Auth.js v5, Resend, React Email, Shadcn/ui to build your next big thing.",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
-    twitter: "https://twitter.com/miickasmt",
-    github: "https://github.com/mickasmt/next-auth-roles-template",
+    twitter: "https://twitter.com/",
+    github: "https://github.com/ahmetozalp",
   },
-  mailSupport: "support@next-starter.fake",
+  mailSupport: "iletisim@ahmetozalp.net",
 };
 
 export const footerLinks: SidebarNavItem[] = [
   {
-    title: "Company",
+    title: "Linkler",
     items: [
-      { title: "About", href: "#" },
-      { title: "Enterprise", href: "#" },
+      { title: "Hakkımda", href: "#" },
+      { title: "Girişimler", href: "#" },
       { title: "Terms", href: "/terms" },
       { title: "Privacy", href: "/privacy" },
     ],
   },
   {
-    title: "Product",
+    title: "Ürünler",
     items: [
       { title: "Security", href: "#" },
       { title: "Customization", href: "#" },
@@ -36,7 +36,7 @@ export const footerLinks: SidebarNavItem[] = [
     ],
   },
   {
-    title: "Docs",
+    title: "Dökümantasyon",
     items: [
       { title: "Introduction", href: "#" },
       { title: "Installation", href: "#" },
